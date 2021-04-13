ed62426a287887b9f521866b569f2c62e5ae7c71
demidraksin <greatraksin@icloud.com>
2021-04-13 11:09:56 +0300

    Adding app configuration❄️

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = 'ugadai-parol'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+# db = SQLAlchemy(app)
 
 from routes import *
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port='8080')
--- routes.py
+++ routes.py
@@ -1,6 +1,12 @@
 from app import app
 from flask import render_template
 
+
 @app.route('/')
 def index_page():
     return render_template('index.html')
+
+
+@app.route('/register')
+def register_page():
+    return render_template('register.html')
